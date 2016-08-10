37b7c38319fcc0c420f43819c388ff2313f30326
roman_yuhimchuk <roman.yuhimchuk@beetroot.se>
2016-08-10 18:18:48 +0300

    final commit

--- app/components/app.js
+++ app/components/app.js
@@ -8,7 +8,7 @@ var App = React.createClass({
 						display : "flex",
 						flexDirection: "row",
 						justifyContent: "space-around",
-						backgroundColor : this.props.number%2 === 0 ? "#ECF4F7" : "#98BFF0",
+						backgroundColor : this.props.number % 2 === 0 ? "#ECF4F7" : "#98BFF0",
 					},
 					number: {
 						width : "10%"
@@ -17,7 +17,7 @@ var App = React.createClass({
 						width: "30%"
 					},
 					img : {
-						background: "url(" + arr.img + ")",
+						backgroundImage: "url(" + arr.img + ")",
 						backgroundSize: "contain",
 						backgroundPosition: "center center",
 						width: 100,
@@ -28,8 +28,8 @@ var App = React.createClass({
 		return(
 			<li style={listStyle.li}> 
 				<div style={listStyle.number} >{this.props.number}</div>
-				<div style={listStyle.textBlocks} ><div style={listStyle.img} ></div>{arr.username}</div>
-				<div cstyle={listStyle.textBlocks} >{arr.recent}</div>
+				<div style={listStyle.textBlocks} > <a href={'https://www.freecodecamp.com/'+arr.username} ><div style={listStyle.img} ></div>{arr.username}</a></div>
+				<div style={listStyle.textBlocks} >{arr.recent}</div>
 				<div style={listStyle.textBlocks} >{arr.alltime}</div>
 			</li>
 		);
--- app/index.js
+++ app/index.js
@@ -4,31 +4,73 @@ var App = require('./components/App');
 
 var Index = React.createClass({
   getInitialState: function () {
-    return { players: {} };
+    return { 
+      players: {},
+      players30: {},
+      players100: {} };
   },
 
    componentDidMount: function() {
     this.serverRequest = $.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent', function (result) {
       this.setState({
         players: result,
+        players30: result
       });
     }.bind(this));
+
+    this.serverRequest1 = $.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', function (result) {
+      console.log("work");
+      this.setState({
+        players100: result,
+      });
+    }.bind(this));
+  },
+  recentFilter: function(){
+    this.setState({
+        players: this.state.players30
+    });
+  },
+  allFilter: function(){
+        this.setState({
+        players: this.state.players100
+    });
   },
 
   componentWillUnmount: function() {
     this.serverRequest.abort();
+    this.serverRequest1.abort();
   },
 
   render: function () {
-    var arr = this.state.players;
-  	console.log(this.state.players);
-   
+    let arr = this.state.players,
+        listStyle = {
+          li : {
+            display : "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            backgroundColor : "darkblue",
+            color: "white"
+          },
+          number: {
+            width : "10%"
+          },
+          textBlocks: {
+            width: "30%"
+          },
+        };
+
     if (!Array.isArray(arr)){
       arr = [];
     }
 
   	return(	
   	<ul>
+      <li style={listStyle.li}> 
+        <div style={listStyle.number} >Position</div>
+        <div style={listStyle.textBlocks} > Name </div>
+        <div style={listStyle.textBlocks} onClick={this.recentFilter} >Recent</div>
+        <div style={listStyle.textBlocks} onClick={this.allFilter} >Alltime</div>
+      </li>
       {
          arr.map((el,i)=>{
              return <App usersObject = {el} key={i+1} number={i+1} />  
